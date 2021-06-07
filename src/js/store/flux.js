9b3d5005cb57609d5d99231b0aa33059f51a49cf
Antoniya <anemtserova@gmail.com>
2021-06-07 13:39:23 +0000

    first commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,10 +2,46 @@ const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			getFetch: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/agenda_2021")
+					.then(response => {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(data => setStore({ contacts: data }))
+					.catch(err => console.log("There was a following error: " + err));
+			},
+
+			postFetch: contact => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						full_name: contact.name,
+						email: contact.email,
+						agenda_slug: "agenda_2021",
+						address: contact.address,
+						phone: contact.phone
+					})
+				})
+					.then(response => {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(data => console.log(data))
+					.catch(err => console.log("There was a following error: " + err));
+			}
 		}
 	};
 };
--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { GlobalState } from "../store/appContext";
 
 export const AddContact = () => {
+	const { StorageEvent, actions } = useContext(GlobalState);
+	const [contact, setContact] = useState({ name: null, email: null, address: null, phone: null });
+	const handleInput = e => {
+		// console.log(e.target);
+		setContact({ ...contact, [e.target.name]: e.target.value });
+	};
 	return (
 		<div className="container">
 			<div>
@@ -9,21 +16,48 @@ export const AddContact = () => {
 				<form>
 					<div className="form-group">
 						<label>Full Name</label>
-						<input type="text" className="form-control" placeholder="Full Name" />
+						<input
+							onChange={handleInput}
+							type="text"
+							className="form-control"
+							name="name"
+							placeholder="Full Name"
+						/>
 					</div>
 					<div className="form-group">
 						<label>Email</label>
-						<input type="email" className="form-control" placeholder="Enter email" />
+						<input
+							onChange={handleInput}
+							type="email"
+							className="form-control"
+							name="email"
+							placeholder="Enter email"
+						/>
 					</div>
 					<div className="form-group">
 						<label>Phone</label>
-						<input type="phone" className="form-control" placeholder="Enter phone" />
+						<input
+							onChange={handleInput}
+							type="phone"
+							className="form-control"
+							name="address"
+							placeholder="Enter phone"
+						/>
 					</div>
 					<div className="form-group">
 						<label>Address</label>
-						<input type="text" className="form-control" placeholder="Enter address" />
+						<input
+							onChange={handleInput}
+							type="text"
+							className="form-control"
+							name="phone"
+							placeholder="Enter address"
+						/>
 					</div>
-					<button type="button" className="btn btn-primary form-control">
+					<button
+						onClick={() => actions.postFetch(contact)}
+						type="button"
+						className="btn btn-primary form-control">
 						save
 					</button>
 					<Link className="mt-3 w-100 text-center" to="/">
